4ae665d9c99ea3cef81710bc881b309c90225bc9
SheikhArbab <arbabzafar4444@gmail.com>
2023-08-05 13:33:53 +0500

    tr

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
     speech.voice = voices[0];
 
-    voices.forEach((voice, i) => (voiceSelect.options[i]) = new Option(voice.name, i));
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
 
 voiceSelect.addEventListener('change', () => {
@@ -14,58 +14,39 @@ voiceSelect.addEventListener('change', () => {
 });
 
 document.querySelector('button').addEventListener('click', () => {
-    speech.text = document.querySelector('textarea').value;
-    window.speechSynthesis.speak(speech);
+    const textarea = document.querySelector('textarea');
+    const textToTranslate = textarea.value;
 
-    // Create an audio element
-    const audio = document.createElement('audio');
-
-    // Listen for the speech to end
-    speech.addEventListener('end', () => {
-        // Create a blob from the speech synthesis data
-        const blob = new Blob([new XMLSerializer().serializeToString(speech)], { type: 'text/xml' });
-
-        // Create a URL for the blob
-        const url = URL.createObjectURL(blob);
-
-        // Set the audio source and enable controls
-        audio.src = url;
-        audio.controls = true;
-
-        // Create a download link
-        const downloadLink = document.createElement('a');
-        downloadLink.href = url;
-        downloadLink.download = 'speech.wav'; // Change the file name and extension if desired
-        downloadLink.innerText = 'Download Speech';
-
-        // Append the audio and download link to the document body
-        document.body.appendChild(audio);
-        document.body.appendChild(downloadLink);
+    googleTranslate(textToTranslate, speech.lang, translatedText => {
+        speech.text = translatedText;
+        window.speechSynthesis.speak(speech);
     });
 });
 
+function googleTranslate(text, targetLang, callback) {
+    const googleTranslateUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=${targetLang}&dt=t&q=${encodeURIComponent(text)}`;
+    
+    fetch(googleTranslateUrl)
+        .then(response => response.json())
+        .then(data => {
+            const translatedText = data[0][0][0];
+            callback(translatedText);
+        })
+        .catch(error => {
+            console.error('Translation error:', error);
+            callback(text); // Fallback to original text
+        });
+}
 
-document.querySelector('.download').addEventListener('click', () => {
-    // Perform actions when the "Download" link is clicked
-    // For example, you can show a success message or perform additional tasks
-
-    // Optionally, you can remove the download link and audio element from the document
-    document.body.removeChild(audio);
-    document.body.removeChild(downloadLink);
-});
-
-
-
-
-
-// google api
-
-function googleTranslateElementInit(){
-
+// Google Translate API initialization
+function googleTranslateElementInit() {
     new google.translate.TranslateElement(
-        {pageLanguage: 'en'},
+        { pageLanguage: 'en' },
         'google_translate_element'
     );
-    
-    }
-    
+
+    document.querySelector('.goog-te-combo').addEventListener('change', (event) => {
+        const selectedLang = event.target.value;
+        speech.lang = selectedLang;
+    });
+}
